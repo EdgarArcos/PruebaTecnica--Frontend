1e1b37d777453bde114b8defbd9c40204e25555b
Edgar Arcos <edgararcosmartin@gmail.com>
2023-05-23 21:33:06 +0100

    fixed new banner

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -9,7 +9,7 @@ export default function Navbar() {
     if (isAuthenticated === false) {
         return (
             <>
-                <nav className=" dark:bg-gray-800 h-32">
+                <nav className="dark:bg-gray-800 h-32">
                     <div className=" px-4 py-1">
                         <div className=" flex items-center">
                             <img src="src\assets\gifed-low-resolution-color-logo-removebg-preview.png" alt="Logo" className=' h-32' />
@@ -21,8 +21,8 @@ export default function Navbar() {
                                     <Link to={"/YourCreations"} className="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 shadow-lg shadow-purple-500/50 dark:shadow-lg dark:shadow-purple-800/80 font-medium rounded-lg text-xl px-3 py-2.5 text-center mb-2">Your creations</Link>
                                 </li>
                                 <li>
-                                    <BsSearch className="absolute block pt-2 pl-2" />
-                                    <input className='pb-1 block py-1   text-xl text-gray-900 border border-gray-300 rounded-lg bg-gray-50  pl-8 relative' placeholder='Search...'></input>
+                                    <BsSearch className=' absolute block pt-2 pl-2  ' />
+                                    <input className=' pb-1 block py-1   text-xl text-gray-900 border border-gray-300 rounded-lg bg-gray-50  pl-8' placeholder='Search...'></input>
                                 </li>
                             </ul>
                             <div className='pl-80'>
@@ -30,7 +30,6 @@ export default function Navbar() {
                                     <LoginButton />
                                 </div>
                             </div>
-
                         </div>
                     </div>
                 </nav>
