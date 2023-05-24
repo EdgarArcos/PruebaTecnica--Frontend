86884ded0562d10b2f5e7430ddc74c0e44cf5599
Edgar Arcos <edgararcosmartin@gmail.com>
2023-05-24 11:23:15 +0100

    Add Copied and Copied toaster

--- src/components/Cards/PostCard.jsx
+++ src/components/Cards/PostCard.jsx
@@ -1,21 +1,27 @@
-export default function PostCard({ post }) {
+import { toast, Toaster } from "react-hot-toast";
+import { CopyToClipboard } from "react-copy-to-clipboard";
 
-    const copy = (post) => {
-        console.log(post.image.url);
+export default function PostCard({ post }) {
+    const copyToast = () => {
+        toast.success('!Copiado¡', {
+            position: "bottom-right"
+        })
     }
 
     return (
         <div className="text-white">
-            <div onClick={() => { copy(post) }} className=" rounded-2xl bg-zinc-800 hover:bg-zinc-700 hover:cursor-pointer ">
-                <div className=" px-12 py-5">
-                    <div className="text-center">
-                        <h2 className=" break-all text-xl uppercase">{post.title}</h2>
-                    </div>
-                    <div className="mt-6">
-                        <img className="rounded-xl h-60max-w-full" src={post.image.url} />
+            <CopyToClipboard text={post.image.url}>
+                <div onClick={() => copyToast()} className=" rounded-2xl bg-zinc-800 hover:bg-zinc-700 hover:cursor-pointer ">
+                    <div className=" px-12 py-5">
+                        <div className="text-center">
+                            <h2 className=" break-all text-xl uppercase">{post.title}</h2>
+                        </div>
+                        <div className="mt-6">
+                            <img className="rounded-xl h-60max-w-full" src={post.image.url} />
+                        </div>
                     </div>
                 </div>
-            </div>
+            </CopyToClipboard>
         </div>
     )
 }
